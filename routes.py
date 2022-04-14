1c8b8bc7687f0c4ed1aa275cf848178a0caf2892
desideri <mr.desipeli@gmail.com>
2022-04-14 15:35:08 +0300

    Added recipe search with options

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from app import app
 from app import db
 from flask import redirect, render_template, request, session
+from sqlalchemy import asc, desc
 import string
 
 @app.route("/")
@@ -81,8 +82,56 @@ def user_info(name):
     result = db.session.execute(sql, {"uname":name}).fetchall()
     return render_template("linklist.html", page_header=name, list=result, direction="/users/"+name+"/")
 
-@app.route("/users/<string:name>/<string:recipe>")
-def recipe(name, recipe):
-    sql = "SELECT I.name, I.amount, Ins.text FROM Instructions Ins, Ingredients I, Recipes R, Users U WHERE U.username=:uname AND R.name=:rname AND I.recipe_id=R.id AND R.user_id=U.id AND Ins.recipe_id=R.id"
-    result = db.session.execute(sql, {"uname":name, "rname":recipe}).fetchall()
-    return render_template("recipe.html", recipe_name=recipe, page_header="Ingredients", ingredients=result, instructions=result)
+#@app.route("/users/<string:name>/<string:recipe>")
+#def recipe(name, recipe):
+#    sql = "SELECT I.name, I.amount, Ins.text FROM Instructions Ins, Ingredients I, Recipes R, Users U WHERE U.username=:uname AND R.name=:rname AND I.recipe_id=R.id AND R.user_id=U.id AND Ins.recipe_id=R.id"
+#    result = db.session.execute(sql, {"uname":name, "rname":recipe}).fetchall()
+#    return render_template("recipe.html", recipe_name=recipe, page_header="Ingredients", ingredients=result, instructions=result)
+
+@app.route("/recipe/<string:recipe_id>")
+def recipe(recipe_id):
+    sql = "SELECT I.name, I.amount, R.name FROM  Ingredients I, Recipes R WHERE R.id=:recipe_id AND I.recipe_id=:recipe_id"
+    result = db.session.execute(sql, {"recipe_id":recipe_id}).fetchall()
+    sql = "SELECT I.text FROM Instructions I WHERE recipe_id=:recipe_id"
+    instructions = db.session.execute(sql, {"recipe_id":recipe_id}).fetchone()
+    if instructions == None:
+        instructions = ["Missing instructions"]
+    print("ins", instructions)
+    print("res", result)
+    return render_template("recipe.html", recipe_name=result[0][2], page_header="Ingredients", ingredients=result, instructions=instructions[0])
+
+@app.route("/recipe_search", methods=["POST", "GET"])
+def recipe_search():
+    if request.method == "GET":
+        sql = "SELECT id, name FROM Recipes"
+        result = db.session.execute(sql).fetchall()
+        return render_template("recipe_list.html", page_header="Recipes", direction="/recipe/", list=result)
+    recipe_name = request.form["recipe_name"]
+    username = request.form["username"]
+    active_time = request.form["active_time"]
+    passive_time = request.form["passive_time"]
+    order_name = request.form["order_name"]
+
+
+    # Check if int and if times are empty = no limit
+    try:
+        int(active_time)
+    except:
+        if active_time == "":
+            active_time = 9999 
+        else: active_time = 0
+    try:
+        int(passive_time)
+    except:
+        if passive_time == "":
+            passive_time = 9999
+        else: passive_time = 0
+
+    sql_user = "(SELECT username FROM Users WHERE username LIKE :username)"
+    if order_name == "1":
+        sql = "SELECT R.id, R.name, U.username FROM Recipes R, Users U WHERE R.name LIKE :r_name AND U.username LIKE :username AND U.id = R.user_id AND R.active_time<=:active_time AND R.passive_time<=:passive_time ORDER BY R.name DESC"
+    else:
+        sql = "SELECT R.id, R.name, U.username FROM Recipes R, Users U WHERE R.name LIKE :r_name AND U.username LIKE :username AND U.id = R.user_id AND R.active_time<=:active_time AND R.passive_time<=:passive_time ORDER BY R.name ASC"
+    result = db.session.execute(sql, {"r_name":"%"+recipe_name+"%", "active_time":active_time, "passive_time":passive_time, "username":"%"+username+"%"}).fetchall()
+    print(result, sql)
+    return render_template("recipe_list.html", page_heade="Recipes", direction="/recipe/", list=result)
