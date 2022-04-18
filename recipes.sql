bdd66406c14355130f5a05573c6a28d051f8ffb5
desideri <mr.desipeli@gmail.com>
2022-04-18 12:22:26 +0300

    Added table for comments

--- recipes.sql
+++ recipes.sql
@@ -2,3 +2,5 @@ CREATE TABLE Users(id SERIAL PRIMARY KEY, username TEXT, password TEXT, admin BO
 CREATE TABLE Recipes(id SERIAL PRIMARY KEY, name TEXT, user_id INTEGER REFERENCES Users (id), difficulty INTEGER, active_time INTEGER, passive_time INTEGER);
 CREATE TABLE Ingredients (id SERIAL PRIMARY KEY, recipe_id INTEGER REFERENCES Recipes (id),name TEXT, amount TEXT);
 CREATE TABLE Instructions (id SERIAL PRIMARY KEY, recipe_id INTEGER REFERENCES Recipes (id), text TEXT);
+
+CREATE TABLE Comments(id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES Users (id), recipe_id INTEGER REFERENCES Recipes (id), text TEXT, date DATE);
