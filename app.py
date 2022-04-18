e32d310ac19badb0189b0d0443c80c43a26aac29
desideri <mr.desipeli@gmail.com>
2022-04-18 12:44:01 +0300

    javascript test

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 #sk = secrets.token_hex(16)
 #sk = "1234"
 app.secret_key = getenv("SECRET_KEY")
+#app.secret_key = sk
 #app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///desideri"
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
