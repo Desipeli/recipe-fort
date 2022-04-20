0426141c6507e5e406a8eae4ccd4df6f0d44caa3
desideri <mr.desipeli@gmail.com>
2022-04-20 16:09:35 +0300

    Moved db to separate module

--- app.py
+++ app.py
@@ -6,11 +6,8 @@ from os import getenv
 
 app = Flask(__name__)
 #sk = secrets.token_hex(16)
-#sk = "1234"
-app.secret_key = getenv("SECRET_KEY")
-#app.secret_key = sk
-#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///desideri"
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-db = SQLAlchemy(app)
+sk = "1234"
+#app.secret_key = getenv("SECRET_KEY")
+app.secret_key = sk
 
 import routes
