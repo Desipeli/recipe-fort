4ddf613a645eaf0abb3db0d0ed66b5fad4120288
desideri <mr.desipeli@gmail.com>
2022-04-20 09:23:16 +0300

    Dates to timestamps

--- recipes.sql
+++ recipes.sql
@@ -1,5 +1,5 @@
-CREATE TABLE Users(id SERIAL PRIMARY KEY, username TEXT, password TEXT, admin BOOLEAN, date DATE);
-CREATE TABLE Recipes(id SERIAL PRIMARY KEY, name TEXT, user_id INTEGER REFERENCES Users (id), difficulty INTEGER, active_time INTEGER, passive_time INTEGER, date DATE);
+CREATE TABLE Users(id SERIAL PRIMARY KEY, username TEXT, password TEXT, admin BOOLEAN, timestamp TIMESTAMP);
+CREATE TABLE Recipes(id SERIAL PRIMARY KEY, name TEXT, user_id INTEGER REFERENCES Users (id), difficulty INTEGER, active_time INTEGER, passive_time INTEGER, meal_type TEXT, timestamp TIMESTAMP);
 CREATE TABLE Ingredients (id SERIAL PRIMARY KEY, recipe_id INTEGER REFERENCES Recipes (id),name TEXT, amount TEXT);
 CREATE TABLE Instructions (id SERIAL PRIMARY KEY, recipe_id INTEGER REFERENCES Recipes (id), text TEXT);
 
