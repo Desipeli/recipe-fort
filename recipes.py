f0f61ff0181e7f3a5db6c849d020717d5d32234f
desideri <mr.desipeli@gmail.com>
2022-04-20 18:07:46 +0300

    Moved recipe logic to separete module

---
+++ recipes.py
@@ -0,0 +1,143 @@
+from db import db
+from flask import session
+from datetime import datetime
+import math
+import meal_categories
+
+def recipe(recipe_id):
+    sql = "SELECT I.name, I.amount, R.name FROM  Ingredients I, Recipes R WHERE R.id=:recipe_id AND I.recipe_id=:recipe_id"
+    result = db.session.execute(sql, {"recipe_id":recipe_id}).fetchall()
+    sql = "SELECT I.text FROM Instructions I WHERE recipe_id=:recipe_id"
+    instructions = db.session.execute(sql, {"recipe_id":recipe_id}).fetchone()
+    if instructions == None:
+        instructions = ["Missing instructions"]
+    return (result[0][2], result, instructions[0])
+    #return result, instructions
+    #return render_template("recipe.html", recipe_name=result[0][2], ingredients=result, instructions=instructions[0])
+
+def recipe_search_GET():
+    sql = "SELECT R.id, R.name, U.username FROM Recipes R, Users U WHERE R.user_id=U.id"
+    return db.session.execute(sql).fetchall()
+
+def recipe_search_POST(recipe_name, username, active_time, passive_time, order_name, difficulty, meal_type):
+    if difficulty == "": difficulty = 3
+    meal_type_column = "meal_type"
+    # Check if int and if times are empty = no limit
+    try:
+        int(active_time)
+    except:
+        if active_time == "":
+            active_time = math.inf
+    try:
+        int(passive_time)
+    except:
+        if passive_time == "":
+            passive_time = math.inf
+    sql = "SELECT R.id, R.name, U.username FROM Recipes R, Users U WHERE R.name LIKE :r_name AND U.username LIKE :username AND U.id = R.user_id AND R.active_time<=:active_time AND R.passive_time<=:passive_time AND difficulty<=:difficulty"
+    if meal_type in meal_categories.meal_types:
+        sql += " AND meal_type=:meal_type"
+    if order_name == "0":
+        sql += " ORDER BY R.name ASC"
+    elif order_name == "1":
+        sql += " ORDER BY R.name DESC"
+    elif order_name == "2":
+        sql += " ORDER BY R.timestamp DESC"
+    elif order_name == "3":
+        sql += " ORDER BY R.timestamp ASC"
+    result = db.session.execute(sql, {"r_name":"%"+recipe_name+"%", "active_time":active_time, "passive_time":passive_time, "username":"%"+username+"%", "difficulty":difficulty, "meal_type":meal_type}).fetchall()
+    return result
+
+def check_recipe(recipe_name, active_time, passive_time, ingredients, amounts, units, instructions, difficulty, meal_type):
+    meal_types = meal_categories.meal_types
+    difficulty_error = ""
+    meal_type_error = ""
+    recipe_name_error = ""
+    active_time_error = ""
+    passive_time_error = ""
+    ingredient_error = ""
+    amount_error = ""
+    unit_error = ""
+    instructions_error = ""
+    error = False
+    if len(recipe_name) == 0 or len(recipe_name) > 100:
+        error = True
+        recipe_name_error = "Recipe name must be 1-100 characters long"
+    try:
+        int(active_time)
+    except:
+        error = True
+        active_time_error = "Time must be integer"
+    try:
+        int(passive_time)
+    except:
+        error = True
+        passive_time_error = "Time must be integer"
+    try:
+        int(difficulty)
+    except:
+        error = True
+        difficulty_error = "Difficulty must be set 0-3"
+    if meal_type not in meal_types:
+        error = True
+        meal_type_error = "Select correct category"
+    for i in ingredients:
+        if len(i) == 0 or len(i) > 100:
+            error = True
+            ingredient_error = "ingredients must be 1-100 characters long"
+    for a in amounts:
+        try:
+            float(a)
+        except:
+            error = True
+            amount_error = "Amount must be a number"
+    for u in units:
+        if len(u) > 100:
+            error = True
+            unit_error = "Units must be 0-100 characters long"
+    if len(instructions) > 10000:
+        error = True
+        instructions_error = "Instructions must be <= 10000 characters long"
+    if error:
+        return recipe_name_error, active_time_error, passive_time_error, ingredient_error, amount_error, unit_error, instructions_error, difficulty_error, meal_type_error
+    return None
+
+
+def create_recipe(recipe_name, active_time, passive_time, ingredients, amounts, units, instructions, difficulty, meal_type):
+    try:
+        sql_user_id = "SELECT id FROM Users WHERE username =:uname"
+        user_id = db.session.execute(sql_user_id, {"uname":session["username"]}).fetchone()[0]
+        dt = datetime.now()
+        sql_create_recipe = "INSERT INTO Recipes (name, user_id, difficulty, active_time, passive_time, meal_type, timestamp) VALUES (:recipe_name, :user_id, :difficulty, :active_time, :passive_time, :meal_type, :timestamp)"
+        db.session.execute(sql_create_recipe, {"recipe_name":recipe_name, "user_id":user_id, "difficulty":difficulty, "active_time":active_time, "passive_time":passive_time, "meal_type":meal_type, "timestamp":dt})
+        db.session.commit()
+        sql_recipe_id = "SELECT id FROM Recipes WHERE name=:recipe_name AND user_id=:user_id AND timestamp=:timestamp"
+        recipe_id = db.session.execute(sql_recipe_id, {"recipe_name":recipe_name, "user_id":user_id, "timestamp":dt}).fetchone()[0]
+        for i in range(len(ingredients)):
+            i_name = ingredients[i]
+            i_amount = str(amounts[i]) + " "  + units[i]
+            sql_insert_ingredients = "INSERT INTO Ingredients (recipe_id, name, amount) VALUES (:recipe_id, :name, :amount)"
+            db.session.execute(sql_insert_ingredients, {"recipe_id":recipe_id, "name":i_name, "amount":i_amount})
+        sql_instructions = "INSERT INTO Instructions (recipe_id, text) VALUES (:recipe_id, :text)"
+        db.session.execute(sql_instructions, {"recipe_id":recipe_id, "text":instructions})
+        db.session.commit()
+        return True
+    except:
+        return False
+
+def add_ingredient(ingredients, amounts, units):
+    if ingredients == None:
+        ingredients = []
+        units = []
+        amounts = []
+    ingredients.append(" ")
+    amounts.append(0)
+    units.append(" ")
+    return ingredients, amounts, units
+
+def remove_ingredient(ingredients, amounts, units):
+    if len(ingredients) > 0:
+        ingredients.pop()
+        amounts.pop()
+        units.pop()
+    return ingredients, amounts, units
+    
--- routes.py
+++ routes.py
@@ -1,12 +1,9 @@
 from app import app
-from db import db
 from flask import redirect, render_template, request, session
-import string
-import math
 import meal_categories
 from datetime import datetime
 from werkzeug.security import check_password_hash, generate_password_hash
-import users
+import users, recipes
 
 @app.route("/")
 def index():
@@ -24,6 +21,7 @@ def login():
 
 @app.route("/logout")
 def logout():
+    users.logout()
     return redirect(request.referrer)
 
 @app.route("/create_account")
@@ -55,38 +53,16 @@ def register_user():
         creation_message_1 = f"An error occurred during registration, try again later: "
         creation_message_2 = ""
     return render_template("register_user.html", creation_message_1=creation_message_1, creation_message_2=creation_message_2)
-    
-
-#@app.route("/users/<string:name>")
-#def user_info(name):
-#    sql = "SELECT R.name FROM Recipes R, Users U WHERE U.id=R.user_id AND U.username=:uname"
-#    result = db.session.execute(sql, {"uname":name}).fetchall()
-#    return render_template("linklist.html", page_header=name, list=result, direction="/users/"+name+"/")
-
-#@app.route("/users/<string:name>/<string:recipe>")
-#def recipe(name, recipe):
-#    sql = "SELECT I.name, I.amount, Ins.text FROM Instructions Ins, Ingredients I, Recipes R, Users U WHERE U.username=:uname AND R.name=:rname AND I.recipe_id=R.id AND R.user_id=U.id AND Ins.recipe_id=R.id"
-#    result = db.session.execute(sql, {"uname":name, "rname":recipe}).fetchall()
-#    return render_template("recipe.html", recipe_name=recipe, page_header="Ingredients", ingredients=result, instructions=result)
-
 @app.route("/recipe/<string:recipe_id>")
 def recipe(recipe_id):
-    sql = "SELECT I.name, I.amount, R.name FROM  Ingredients I, Recipes R WHERE R.id=:recipe_id AND I.recipe_id=:recipe_id"
-    result = db.session.execute(sql, {"recipe_id":recipe_id}).fetchall()
-    sql = "SELECT I.text FROM Instructions I WHERE recipe_id=:recipe_id"
-    instructions = db.session.execute(sql, {"recipe_id":recipe_id}).fetchone()
-    if instructions == None:
-        instructions = ["Missing instructions"]
-    print("ins", instructions)
-    print("res", result)
-    return render_template("recipe.html", recipe_name=result[0][2], page_header="Ingredients", ingredients=result, instructions=instructions[0])
+    result = recipes.recipe(recipe_id)
+    return render_template("recipe.html", recipe_name=result[0], ingredients=result[1], instructions=result[2])
 
 
 @app.route("/recipe_search", methods=["POST", "GET"])
 def recipe_search():
     if request.method == "GET":
-        sql = "SELECT R.id, R.name, U.username FROM Recipes R, Users U WHERE R.user_id=U.id"
-        result = db.session.execute(sql).fetchall()
+        result = recipes.recipe_search_GET()
         return render_template("recipe_list.html", page_header="Recipes", direction="/recipe/", list=result, meal_types=meal_categories.meal_types)
     recipe_name = request.form["recipe_name"]
     username = request.form["username"]
@@ -94,35 +70,8 @@ def recipe_search():
     passive_time = request.form["passive_time"]
     order_name = request.form["order_name"]
     difficulty = request.form['difficulty']
-    if difficulty == "": difficulty = 0
     meal_type = request.form['meal_type']
-    meal_type_column = "meal_type"
-
-    # Check if int and if times are empty = no limit
-    try:
-        int(active_time)
-    except:
-        if active_time == "":
-            active_time = math.inf
-    try:
-        int(passive_time)
-    except:
-        if passive_time == "":
-            passive_time = math.inf
-
-    sql_user = "(SELECT username FROM Users WHERE username LIKE :username)"
-    sql = "SELECT R.id, R.name, U.username FROM Recipes R, Users U WHERE R.name LIKE :r_name AND U.username LIKE :username AND U.id = R.user_id AND R.active_time<=:active_time AND R.passive_time<=:passive_time AND difficulty<=:difficulty"
-    if meal_type in meal_categories.meal_types:
-        sql += " AND meal_type=:meal_type"
-    if order_name == "0":
-        sql += " ORDER BY R.name ASC"
-    elif order_name == "1":
-        sql += " ORDER BY R.name DESC"
-    elif order_name == "2":
-        sql += " ORDER BY R.timestamp DESC"
-    elif order_name == "3":
-        sql += " ORDER BY R.timestamp ASC"
-    result = db.session.execute(sql, {"r_name":"%"+recipe_name+"%", "active_time":active_time, "passive_time":passive_time, "username":"%"+username+"%", "difficulty":difficulty, "meal_type":meal_type}).fetchall()
+    result = recipes.recipe_search_POST(recipe_name, username, active_time, passive_time, order_name, difficulty, meal_type)
     return render_template("recipe_list.html", page_heade="Recipes", direction="/recipe/", list=result, meal_types=meal_categories.meal_types)
 
 @app.route("/profile/<string:uname>")
@@ -148,89 +97,20 @@ def check_recipe():
     instructions = request.form['instructions']
     difficulty = request.form['difficulty']
     meal_type = request.form['meal_type']
-    difficulty_error = ""
-    meal_type_error = ""
-    recipe_name_error = ""
-    active_time_error = ""
-    passive_time_error = ""
-    ingredient_error = ""
-    amount_error = ""
-    unit_error = ""
-    instructions_error = ""
-    error = False
-    
-    if len(recipe_name) == 0 or len(recipe_name) > 100:
-        error = True
-        recipe_name_error = "Recipe name must be 1-100 characters long"
-    try:
-        int(active_time)
-    except:
-        error = True
-        active_time_error = "Time must be integer"
-    try:
-        int(passive_time)
-    except:
-        error = True
-        passive_time_error = "Time must be integer"
-    try:
-        int(difficulty)
-    except:
-        error = True
-        difficulty_error = "Difficulty must be set 0-3"
-    if meal_type not in meal_types:
-        error = True
-        meal_type_error = "Select correct category"
-    
-    for i in ingredients:
-        if len(i) == 0 or len(i) > 100:
-            error = True
-            ingredient_error = "ingredients must be 1-100 characters long"
-    for a in amounts:
-        try:
-            float(a)
-        except:
-            error = True
-            amount_error = "Amount must be a number"
-    for u in units:
-        if len(u) > 100:
-            error = True
-            unit_error = "Units must be 0-100 characters long"
-    if len(instructions) > 10000:
-        error = True
-        instructions_error = "Instructions must be <= 10000 characters long"
+    error = recipes.check_recipe(recipe_name, active_time, passive_time, ingredients, amounts, units, instructions, difficulty, meal_type)
     if error:
-        return render_template("write_recipe.html", meal_types=meal_categories.meal_types, recipe_name_error=recipe_name_error, active_time_error=active_time_error, passive_time_error=passive_time_error, ingredient_error=ingredient_error, amount_error=amount_error, unit_error=unit_error, difficulty_error=difficulty_error, instructions_error=instructions_error, ingredient_list=ingredients, amount_list=amounts, unit_list=units)
-    sql_user_id = "SELECT id FROM Users WHERE username =:uname"
-    user_id = db.session.execute(sql_user_id, {"uname":session["username"]}).fetchone()[0]
-    dt = datetime.now()
-    sql_create_recipe = "INSERT INTO Recipes (name, user_id, difficulty, active_time, passive_time, meal_type, timestamp) VALUES (:recipe_name, :user_id, :difficulty, :active_time, :passive_time, :meal_type, :timestamp)"
-    db.session.execute(sql_create_recipe, {"recipe_name":recipe_name, "user_id":user_id, "difficulty":difficulty, "active_time":active_time, "passive_time":passive_time, "meal_type":meal_type, "timestamp":dt})
-    db.session.commit()
-    sql_recipe_id = "SELECT id FROM Recipes WHERE name=:recipe_name AND user_id=:user_id AND timestamp=:timestamp"
-    recipe_id = db.session.execute(sql_recipe_id, {"recipe_name":recipe_name, "user_id":user_id, "timestamp":dt}).fetchone()[0]
-    for i in range(len(ingredients)):
-        i_name = ingredients[i]
-        i_amount = str(amounts[i]) + " "  + units[i]
-        sql_insert_ingredients = "INSERT INTO Ingredients (recipe_id, name, amount) VALUES (:recipe_id, :name, :amount)"
-        db.session.execute(sql_insert_ingredients, {"recipe_id":recipe_id, "name":i_name, "amount":i_amount})
-    sql_instructions = "INSERT INTO Instructions (recipe_id, text) VALUES (:recipe_id, :text)"
-    db.session.execute(sql_instructions, {"recipe_id":recipe_id, "text":instructions})
-    db.session.commit()
-    return redirect("/")
+        return render_template("write_recipe.html", meal_types=meal_categories.meal_types, recipe_name_error=error[0], active_time_error=error[1], passive_time_error=error[2], ingredient_error=error[3], amount_error=error[4], unit_error=error[5], instructions_error=error[6], difficulty_error=error[7], meal_type_error=error[8], ingredient_list=ingredients, amount_list=amounts, unit_list=units)
+    if recipes.create_recipe(recipe_name, active_time, passive_time, ingredients, amounts, units, instructions, difficulty, meal_type):
+        return redirect("/")
+    else:
+        return render_template("write_recipe.html", meal_types=meal_categories.meal_types, recipe_name_error="DATABASE ERROR IN RECIPE CREATION", active_time_error=error[1], passive_time_error=error[2], ingredient_error=error[3], amount_error=error[4], unit_error=error[5], instructions_error=error[6], difficulty_error=error[7], meal_type_error=error[8], ingredient_list=ingredients, amount_list=amounts, unit_list=units)
 
 @app.route("/add_ingredient", methods=["POST"])
 def add_ingredient():
     ingredients = request.form.getlist('ingredient')
     amounts = request.form.getlist('amount')
     units = request.form.getlist('unit')
-    print(ingredients, amounts, units)
-    if ingredients == None:
-        ingredients = []
-        units = []
-        amounts = []
-    ingredients.append(" ")
-    amounts.append(0)
-    units.append(" ")
+    ingredients, amounts, units = recipes.add_ingredient(ingredients, amounts, units)
     return render_template("write_recipe.html", meal_types=meal_categories.meal_types, ingredient_list=ingredients, amount_list=amounts, unit_list=units)
 
 @app.route("/remove_ingredient", methods=["POST"])
@@ -238,8 +118,5 @@ def remove_ingredient():
     ingredients = request.form.getlist('ingredient')
     amounts = request.form.getlist('amount')
     units = request.form.getlist('unit')
-    if len(ingredients) > 0:
-        ingredients.pop()
-        amounts.pop()
-        units.pop()
+    ingredients, amounts, units = recipes.remove_ingredient(ingredients, amounts, units)
     return render_template("write_recipe.html", meal_types=meal_categories.meal_types, ingredient_list=ingredients, amount_list=amounts, unit_list=units)
