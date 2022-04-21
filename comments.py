17924a1d7b6d2d256f9e773e25863710f664b158
desideri <mr.desipeli@gmail.com>
2022-04-21 16:55:19 +0300

    Can now get comments for specific recipe

--- comments.py
+++ comments.py
@@ -5,3 +5,8 @@ def post_comment_to_recipe(user_id, recipe_id, comment):
     sql = "INSERT INTO Comments (user_id, recipe_id, text, date) VALUES (:user_id, :recipe_id, :comment, :date)"
     db.session.execute(sql, {"user_id":user_id, "recipe_id":recipe_id, "comment":comment, "date":datetime.now()})
     db.session.commit()
+
+def get_comments_for_recipe(recipe_id):
+    sql = "SELECT U.username, C.text, C.date FROM Comments C, Users U WHERE C.recipe_id=:recipe_id AND C.user_id=U.id ORDER BY date DESC"
+    return db.session.execute(sql, {"recipe_id":recipe_id}).fetchall()
+    
